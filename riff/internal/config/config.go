30fd3091431253bea85416384118c9d88902e219
kabeer11000 <kabeer11000@gmail.com>
2026-07-28 19:42:34 +0500

    Added more advanced proxy configuration

--- riff/internal/config/config.go
+++ riff/internal/config/config.go
@@ -28,7 +28,7 @@ func Load() Config {
 		TursoURL:         env("TURSO_DATABASE_URL", ""),
 		TursoToken:       env("TURSO_AUTH_TOKEN", ""),
 		YTDLPPath:        env("YTDLP_PATH", ""),
-		YTDLPCookiesFile: env("YTDLP_COOKIES_FILE", ""),
+		YTDLPCookiesFile: env("YTDLP_COOKIES_FILE", "/data/yt-cookies.txt"),
 		ProxyListURL:     env("PROXY_LIST_URL", "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/socks5/data.json"),
 		CacheMaxTTL:      envDuration("CACHE_MAX_TTL", 5*time.Minute),
 		CacheMetadataTTL: envDuration("CACHE_METADATA_TTL", time.Hour),
